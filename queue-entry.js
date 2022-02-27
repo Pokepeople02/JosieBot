7ea6a7002ed70a5f3cfad0eeb39ee0e936f759ce
Pokepeople02 <s08422214@gmail.com>
2022-02-27 10:53:50 -0500

    Minor adjustment to YT searching and handling of empty results

--- queue-entry.js
+++ queue-entry.js
@@ -40,13 +40,13 @@ module.exports.QueueEntry = class QueueEntry {
 			case 'search' :
 				let results = await play.search( this.#str, {
 						limit: 1,
-						source: { youtube:'video' }
 				} );
-				
+
 				if( results.length === 0 ) {
 					//Returned no or invalid results
 					this.#type = false;
-					break;
+					console.log( 'Found no results for request' );
+					return;
 				}//end if
 				
 				this.#info = await play.video_info( results[0].url );
