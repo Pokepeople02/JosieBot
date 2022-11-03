56814fa2400d16cd086247e43e2ceeaf7f7be7ec
Pokepeople02 <s08422214@gmail.com>
2022-11-03 16:24:11 -0400

    Implemented /play

--- src/commands/data/play.ts
+++ src/commands/data/play.ts
@@ -1,7 +1,14 @@
-import { ChatInputCommandInteraction, CacheType, SlashCommandBuilder, GuildMember } from "discord.js";
+import { ChatInputCommandInteraction, CacheType, SlashCommandBuilder, Snowflake, ApplicationCommand, Collection, MessagePayload } from "discord.js";
 import { Command } from "../../Command";
-import { GuildContract } from "../../GuildContract";
-import { processNewRequest } from "../execution/Play";
+import { play } from "../execution/Play";
+import { Request } from "../../requests/Request";
+import { BadRequestError } from "../../errors/BadRequestError";
+import { DurationError } from "../../errors/DurationError";
+import { NonVoiceChannelError } from "../../errors/NonVoiceChannelError";
+import { TimeoutError } from "../../errors/TimeoutError";
+import { ResourceUnobtainableError } from "../../errors/ResourceUnobtainableError";
+import { EmbedBuilder } from "@discordjs/builders";
+import { UnresolvedChannelError } from "../../errors/UnresolvedChannelError";
 
 let Play: Command = {
 
@@ -15,11 +22,90 @@ let Play: Command = {
         )
         .toJSON(),
 
-    /**TODO: Unfinished */
-    async execute( interaction: ChatInputCommandInteraction ): Promise<void> {
-        //Check if user is in voice, handle channelId 0 differently
+    /**Determines the appropriate channel  */
+    async execute( interaction: ChatInputCommandInteraction<"cached"> ): Promise<void> {
+        const user = interaction.member;
+        const userVoice = user.voice.channel;
+        let commands: Collection<Snowflake, ApplicationCommand>;
+        let playId: Snowflake, playChannelId: Snowflake, playUserId: Snowflake;
+        let request: Request;
 
-        await processNewRequest( interaction, ( interaction.member! as GuildMember ).voice.channelId! );
+        if ( !userVoice ) {
+            //Get play command IDs
+            commands = await globalThis.client.application!.commands.fetch();
+            playId = commands.filter( command => command.name === "play" ).first()!.id;
+            playChannelId = commands.filter( command => command.name === "play-channel" ).first()!.id;
+            playUserId = commands.filter( command => command.name === "play-user" ).first()!.id;
+
+            await interaction.reply( {
+                embeds: [{
+                    title: "❌ Unable to Add Request",
+                    description: `You need to be in a voice channel to use </play:${playId}>.\nJoin a channel, or use either ` +
+                        `</play-channel:${playChannelId}> or </play-user:${playUserId}> to add requests.`,
+                }],
+            } );
+
+            return;
+        }//end if
+
+        try {
+            await interaction.deferReply();
+
+            request = await play( interaction, userVoice.id );
+
+            await interaction.editReply( {
+                embeds: [{
+                    title: "✅ Added a Request",
+                    description: `Successfully queued [${request.title!}](${request.resourceUrl!}) for your channel.`,
+                    thumbnail: request.thumbnailUrl ? { url: request.thumbnailUrl } : undefined,
+                    fields: [
+                        {
+                            name: request.lengthType,
+                            value: request.lengthFormatted!,
+                            inline: true,
+                        },
+                        {
+                            name: "Uploaded by",
+                            value: request.creator!,
+                            inline: true,
+                        },
+                    ]
+                }],
+            } );
+
+        } catch ( error ) {
+            let replyEmbed = new EmbedBuilder();
+            replyEmbed.setTitle( "❌  Unable to Add Request" );
+
+            globalThis.client.log( `Failed to add request -- ${error}`, interaction );
+
+            if ( error instanceof BadRequestError ) {
+                switch ( error.type ) {
+                    case "invalid":
+                        replyEmbed.setDescription( "Request is invalid and cannot be played. Please try a different request." );
+                        break;
+                    case "unknown":
+                        replyEmbed.setDescription( "Unable to determine what type of request this is. Please try a different request." );
+                        break;
+                    case "unsupported":
+                        replyEmbed.setDescription( "This type of request is not yet supported. Please try a different request." );
+                        break;
+                }//end switch
+            } else if ( error instanceof DurationError )
+                replyEmbed.setDescription( "The desired `start` and `end` don't make sense for this request. Please fix and try again." );
+            else if ( error instanceof UnresolvedChannelError )
+                replyEmbed.setDescription( "Failed to determine what channel to play this request in. Please try a different channel." );
+            else if ( error instanceof TimeoutError )
+                replyEmbed.setDescription( "The source for this request took too long to respond. Please try again." );
+            else if ( error instanceof ResourceUnobtainableError )
+                replyEmbed.setDescription( "Could not obtain necessary info about this request, it may not be valid. Try again, or try a different request." );
+            else {
+                replyEmbed.setDescription( "An unknown error occurred while adding this request. Try again, or try a different request." );
+            }//end if-else
+
+            await interaction.editReply( { embeds: [replyEmbed] } );
+        }//end try-catch
+
         return;
     }//end method execute
 };
--- src/commands/execution/play.ts
+++ src/commands/execution/play.ts
@@ -1,32 +1,35 @@
 import { ChatInputCommandInteraction, Snowflake } from "discord.js";
-import { GuildContract } from "../../GuildContract";
+import { NonVoiceChannelError } from "../../errors/NonVoiceChannelError";
+import { UnresolvedChannelError } from "../../errors/UnresolvedChannelError";
+import { TimeoutError } from "../../errors/TimeoutError";
+import { ResourceUnobtainableError } from "../../errors/ResourceUnobtainableError";
+import { createRequest } from "../../requests/CreateRequest";
 import { Request } from "../../requests/Request";
-import { createRequest } from "../../requests/RequestFactory";
 
-/**TODO: Unfinished */
-export async function processNewRequest( interaction: ChatInputCommandInteraction, channelId: Snowflake ): Promise<void> {
+/**Adds a request to the queue for the guild from which the given interaction originated, to be played in the channel specified.
+ * If the queue for that guild was previously empty, begins playing the request.
+ * @param {ChatInputCommandInteraction} interaction The prompting interaction.
+ * @param {Snowflake} channelId The ID of the channel in which to play the request.
+ * @returns {Promise<Request>} A promise for the request to be added to the queue.
+ * @throws {@link BadRequestError} When the type of request wanted is invalid, unsupported, or unable to be determined.
+ * @throws {@link UnresolvedUserError} When the requesting user's ID is unable to be resolved.
+ * @throws {@link DurationError} When the `start` or `end` durations are not in a valid range for that request.
+ * @throws {@link UnresolvedChannelError} When the target channel ID is unable to be resolved.
+ * @throws {@link NonVoiceChannelError} When the target channel ID resolves to a non-voice channel.
+ * @throws {@link TimeoutError} When retreiving request info takes too long to fulfill.
+ * @throws {@link ResourceUnobtainableError} When an error occurs while retreiving request info.
+ */
+export async function play( interaction: ChatInputCommandInteraction<"cached">, channelId: Snowflake ): Promise<Request> {
     const contract = globalThis.client.contracts.get( interaction.guildId! )!;
+    const input = interaction.options.getString( "request", true );
+    const userId = interaction.member.id;
     let request: Request;
 
-    await interaction.deferReply();
+    request = await createRequest( input, userId, channelId );
 
-    try {
-        request = await createRequest(
-            interaction.options.getString( "request", true ),
-            interaction.member!.user.id,
-            channelId
-        );
-    } catch ( error ) {
-        globalThis.client.log( `Request creation failed: "${error}"`, interaction );
+    console.log( "Adding request now" );
 
-        if ( !interaction.replied )
-            await interaction.editReply( `Unable to play request. ${error}.` );
-        return;
-    }//end try-catch
+    contract.add( request );
 
-    //add to queue
-    //if queue length is 1, play 
-    //reply
-
-    return;
+    return request;
 }//end function play
