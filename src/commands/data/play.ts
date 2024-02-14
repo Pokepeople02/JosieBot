5ae112ccc186124e6aafa9d434c4b2cac1c74c6d
Pokepeople02 <s08422214@gmail.com>
2024-02-13 23:36:12 -0500

    Tweaked error message verbiage

--- src/commands/data/play.ts
+++ src/commands/data/play.ts
@@ -114,7 +114,7 @@ export function getPlayFailedResponseEmbed( error: unknown, ): EmbedBuilder {
                 replyEmbed.setDescription( "This request is age-restricted and the YouTube cookies loaded are for an account below age 18. Please try a different request, ask the bot owner to set cookies for an 18+ YouTube account, or wait until the corresponding account is age 18." );
                 break;
             case "ageRestrictedNoCookies":
-                replyEmbed.setDescription( "This request is age-restricted and no YouTube cookies are available to sign in. Please try a different request, or ask the bot owner to set YouTube cookies and try again." );
+                replyEmbed.setDescription( "This request is age-restricted and no YouTube cookies are available for sign-in. Please try a different request, or ask the bot owner to set YouTube cookies and try again." );
                 break;
             case "unknown":
                 replyEmbed.setDescription( "Could not obtain necessary info about this request, it may not be valid. Please try again or try a different request." );
